e5fc7e200b29c759eecccd574e86e31f90915c12
Phinjock Sherpa <phinjock@gmail.com>
2022-09-15 18:03:14 -0700

    completed loggin/logout, update package.json WIP models folder/seeds

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -22,3 +22,48 @@ router.post("/", async (req, res) => {
 });
 
 // login
+router.post("/login", async (req, res) => {
+  try {
+    const dbUserData = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (!dbUserData) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password. Please try again" });
+      return;
+    }
+    const validPassword = await dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password. Please try again" });
+      return;
+    }
+    req.session.save(() => {
+      req.session.login = true;
+
+      res.status(200).json({ user: dbUserData, message: "You are logged in!" });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+//logout
+route.post("./logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(400).end();
+  }
+});
+
+module.exports = router;
